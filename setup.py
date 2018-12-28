b54ecc08e5691400962d2cd84bb91b18d208e00b
Lauri Himanen <lauri.himanen@aalto.fi>
2018-12-28 10:36:55 +0200

    Initial setup.

---
+++ setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages, Extension
+
+extensions = [
+    # The MBTR C++ extension, wrapped with cython
+    Extension(
+        "dscribe.libmbtr.cmbtrwrapper",
+        [
+            "dscribe/libmbtr/cmbtrwrapper.cpp",
+        ],
+        include_dirs=["dscribe/libmbtr"],
+        extra_compile_args=['-std=c++11'],
+    ),
+]
+
+if __name__ == "__main__":
+    setup(name='test',
+        version='0.1.0',
+        url="https://singroup.github.io/dscribe/",
+        description='A Python package for creating feature transformations in applications of machine learning to materials science.',
+        long_description='A Python package for creating feature transformations in applications of machine learning to materials science.',
+        packages=find_packages(),
+        install_requires=[
+        ],
+        include_package_data=True,  # This ensures that files defined in MANIFEST.in are included
+        ext_modules=extensions,
+        license="Apache License 2.0",
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Developers',
+            'Topic :: Scientific/Engineering :: Physics',
+            'License :: OSI Approved :: Apache Software License',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.6',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.2',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+        ],
+        keywords='descriptor machine learning atomistic structure materials science',
+        python_requires='>=2.6, <4',
+    )
---
+++ test/libmbtr/cmbtrwrapper.pyx
@@ -0,0 +1,67 @@
+# distutils: language = c++
+
+import numpy as np
+from libcpp cimport bool
+from libcpp.vector cimport vector
+from libcpp.map cimport map
+from cmbtr cimport CMBTR
+
+cdef class CMBTRWrapper:
+    cdef CMBTR *thisptr      # hold a C++ instance which we're wrapping
+
+    def __cinit__(self, vector[vector[float]] positions, vector[int] atomic_numbers, map[int,int] atomic_number_to_index_map, int interaction_limit, bool is_local):
+        self.thisptr = new CMBTR(positions, atomic_numbers, atomic_number_to_index_map, interaction_limit, is_local)
+
+    def __dealloc__(self):
+        del self.thisptr
+
+    def get_displacement_tensor(self):
+        return np.array(self.thisptr.getDisplacementTensor(), dtype=np.float32)
+
+    def get_distance_matrix(self):
+        return np.array(self.thisptr.getDistanceMatrix(), dtype=np.float32)
+
+    def get_k1_geoms_and_weights(self, geom_func, weight_func, parameters):
+        """Cython cannot directly provide the keys as tuples, so we have to do
+        the conversion here on the python side.
+        """
+        geom_map, weight_map = self.thisptr.getK1GeomsAndWeightsCython(geom_func, weight_func, parameters);
+        new_geom_map = {}
+        new_weight_map = {}
+
+        for key, value in geom_map.items():
+            new_key = tuple([int(key.decode("utf-8"))])
+            new_geom_map[new_key] = value
+            new_weight_map[new_key] = weight_map[key]
+
+        return new_geom_map, new_weight_map
+
+    def get_k2_geoms_and_weights(self, geom_func, weight_func, parameters):
+        """Cython cannot directly provide the keys as tuples, so we have to do
+        the conversion here on the python side.
+        """
+        geom_map, weight_map = self.thisptr.getK2GeomsAndWeightsCython(geom_func, weight_func, parameters);
+        new_geom_map = {}
+        new_weight_map = {}
+
+        for key, value in geom_map.items():
+            new_key = tuple(int(x) for x in key.decode("utf-8").split(","))
+            new_geom_map[new_key] = value
+            new_weight_map[new_key] = weight_map[key]
+
+        return new_geom_map, new_weight_map
+
+    def get_k3_geoms_and_weights(self, geom_func, weight_func, parameters):
+        """Cython cannot directly provide the keys as tuples, so we have to do
+        the conversion here on the python side.
+        """
+        geom_map, weight_map = self.thisptr.getK3GeomsAndWeightsCython(geom_func, weight_func, parameters);
+        new_geom_map = {}
+        new_weight_map = {}
+
+        for key, value in geom_map.items():
+            new_key = tuple(int(x) for x in key.decode("utf-8").split(","))
+            new_geom_map[new_key] = value
+            new_weight_map[new_key] = weight_map[key]
+
+        return new_geom_map, new_weight_map
