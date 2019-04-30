cfd5ad68cd7d11e1602430ecbe4b0216c890fb87
Lauri Himanen <lauri.himanen@aalto.fi>
2019-04-30 19:36:34 +0300

    Deleted the installation of development dependencies

--- .travis-install-dependencies.sh
+++ .travis-install-dependencies.sh
@@ -1,6 +1,6 @@
 # Development dependencies
 pip install numpy==1.15.4
-pip install -r devrequirements.txt
+#pip install -r devrequirements.txt
 
 # Compile CMBTR extension with cython. The .so file is not generated, as it
 # will be compiled during package setup
@@ -16,7 +16,7 @@ pip install -r devrequirements.txt
 
 #softwareupdate --list
 #softwareupdate -i "Command Line Tools (macOS High Sierra version 10.13) for Xcode-10.1"
-softwareupdate -i --install -a
+#softwareupdate -i --install -a
 
 pkgutil --pkg-info=com.apple.pkg.CLTools_Executables
 pkgutil --pkg-info=com.apple.pkg.DeveloperToolsCLI
